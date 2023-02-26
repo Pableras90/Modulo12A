7f087900106c271b039a01cace5e334bd9bc92e8
Pablo <p.reinaldo90@gmail.com>
2023-02-26 18:38:52 +0100

    26/02 commit

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,5 +1,7 @@
-import { onUpdateField, onSubmitForm } from '../../common/helpers';
+import { onUpdateField, onSubmitForm, onSetError, onSetFormErrors } from '../../common/helpers';
 import { isValidLogin } from './login.api';
+import { formValidation } from './login.validations';
+import { history, routes } from '../../core/router';
 let login = {
     user: '',
     password: '',
@@ -9,21 +11,40 @@ onUpdateField("user", (event) => {
     const value = event.target.value;
     login = {
         ...login,
-        user: value
-    }
-})
+        user: value,
+    };
+    formValidation.validateField('user', login.user).then(result => {
+        onSetError('user', result);
+    })
+});
 
 onUpdateField("password", (event) => {
     const value = event.target.value;
     login = {
         ...login,
         password: value
-    }
+    };
+    formValidation.validateField('password', login.password).then(result => {
+        onSetError('password', result);
+    })
 });
 
+const onNavigate = (isValid) => {
+    if (isValid) {
+        history.push(routes.accountList);
+    } else {
+        alert('Usuario y/o contraseña no válidos')
+    }
+};
+
 onSubmitForm("login-button", () => {
-    isValidLogin(login).then(isValid => {
-        console.log({ isValid });
-    });
+    formValidation.validateForm(login).then(result => {
+        onSetFormErrors(result);
+        if (result.succeeded) {
+            isValidLogin(login).then(isValid => {
+                onNavigate(isValid);
+            });
+        }
+    })
 });
 
