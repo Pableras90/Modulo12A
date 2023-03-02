6e25dd82304110398321b367925b56e093499525
Pablo <p.reinaldo90@gmail.com>
2023-03-02 19:09:19 +0100

    02/03 commit

--- src/pages/account-list/account-list.js
+++ src/pages/account-list/account-list.js
@@ -1,5 +1,9 @@
 import { getAccountList } from './account-list.api';
 import { addAccountRows } from './account-list.helpers';
+import { mapAccountListFromApiToViewModel } from './account-list.mappers'
+import { onUpdateField } from '../../common/helpers';
+import { history } from '../../core/router';
+
 
 /*
 Account{
@@ -11,5 +15,12 @@ Account{
 }
 */
 getAccountList().then(accountList => {
-    addAccountRows(accountList);
+    const viewModelAccountList = mapAccountListFromApiToViewModel(accountList);
+    addAccountRows(viewModelAccountList);
+    viewModelAccountList.forEach(account => {
+        onUpdateField(`select-${account.id}`, event => {
+            const route = event.target.value;
+            history.push(route);
+        })
+    })
 });
