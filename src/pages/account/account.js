e2d9cfbfb74a329a203e4a38e4224b93d0fd052c
Pablo <p.reinaldo90@gmail.com>
2023-03-05 13:30:11 +0100

    05/03 commit

---
+++ src/pages/account/account.js
@@ -0,0 +1,41 @@
+import { onUpdateField, onSubmitForm, onSetError, onSetFormErrors } from '../../common/helpers';
+import { formValidation, FormValidation } from './account.validations';
+
+let account = {
+    id: '',
+    type: '',
+    alias: '',
+};
+
+
+onUpdateField('type', event => {
+    const value = event.target.value;
+    account = {
+        ...account,
+        type: value,
+    };
+    formValidation.validateField('type', account.type).then(result => {
+        onSetError('type', result);
+
+    })
+});
+
+onUpdateField('alias', event => {
+    const value = event.target.value;
+    account = {
+        ...account,
+        alias: value,
+    };
+    formValidation.validateField('alias', account.alias).then(result => {
+        onSetError('alias', result);
+    })
+});
+
+onSubmitForm('save-button', () => {
+    formValidation.validateForm(account).then(result => {
+        onSetFormErrors(result);
+        if (result.succeeded) {
+            console.log({ account });
+        }
+    });
+});
